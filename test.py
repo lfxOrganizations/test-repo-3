c49ad556a3ecbf40a9e27ff525383e79362fbe94
yashgoyal0110 <yg364550@gmail.com>
2026-01-12 15:52:05 +0530

    commit

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
 def test():
     print("This is a test function.")
     return True
-    print("NOT A TES FN")
-    return True
-print("FILE LOADED")
-print(test())
-test()
-print("DONE")
-print(test())
-print("FINAL")
 
-print("RAISING CONFLICTS")
-print("EXTRA LINE")
-print("ANOTHER EXTRA LINE")
